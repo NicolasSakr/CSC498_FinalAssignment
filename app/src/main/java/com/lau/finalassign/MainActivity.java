15b6b784728301817fd8c08c6664d16e14ddf241
unknown <nicolassakr2001@outlook.com>
2022-05-07 01:10:15 +0300

    finalizing

--- app/src/main/java/com/lau/finalassign/MainActivity.java
+++ app/src/main/java/com/lau/finalassign/MainActivity.java
@@ -2,13 +2,65 @@ package com.lau.finalassign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.clist);
+        web = (WebView) findViewById(R.id.web);
+        ArrayList<String> cname = new ArrayList<>();
+        ArrayList<String> clink = new ArrayList<>();
+
+
+        try{
+
+            SQLiteDatabase sql = this.openOrCreateDatabase("cousedb", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS course (course_name VARCHAR, course_link VARCHAR)");
+            //sql.execSQL("INSERT INTO course(course_name, course_link) VALUES ('Mobile Computing', 'https://ionicframework.com/'), ('scripting', 'https://www.regexone.com/'), ('DataBase Management Systems', 'https://www.dev.mysql.com/')");
+
+            Cursor c = sql.rawQuery("Select * from course", null);
+            int c_name_Index = c.getColumnIndex("course_name");
+            int c_link_Index = c.getColumnIndex("course_link");
+            c.moveToFirst();
+
+            int x=0;
+            while(x<c.getCount()){
+                cname.add(c.getString(c_name_Index));
+                clink.add(c.getString(c_link_Index));
+                c.moveToNext();
+                x+=1;
+            }
+            ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, cname);
+            list.setAdapter(adapter);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                web.loadUrl(clink.get(i));
+            }
+        });
     }
+
 }
